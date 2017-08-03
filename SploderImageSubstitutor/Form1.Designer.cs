8cc5940dc1fe00235f43e2ce6e39991e5d9e3452
Deliagwath <PPL3000@Gmail.com>
2017-08-03 16:40:33 +0700

    Skeleton Commit
    Basic UI
    Basic Button Code
    TODO: Fix Base64 Conversion

--- SploderImageSubstitutor/Form1.Designer.cs
+++ SploderImageSubstitutor/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.XMLButton = new System.Windows.Forms.Button();
+            this.ImageButton = new System.Windows.Forms.Button();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.DebugButton = new System.Windows.Forms.Button();
+            this.XMLFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.TextBoxLog = new System.Windows.Forms.TextBox();
+            this.ImageFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // XMLButton
+            // 
+            this.XMLButton.Location = new System.Drawing.Point(13, 13);
+            this.XMLButton.Name = "XMLButton";
+            this.XMLButton.Size = new System.Drawing.Size(116, 23);
+            this.XMLButton.TabIndex = 0;
+            this.XMLButton.Text = "XML Location";
+            this.XMLButton.UseVisualStyleBackColor = true;
+            this.XMLButton.Click += new System.EventHandler(this.XMLButton_Click);
+            // 
+            // ImageButton
+            // 
+            this.ImageButton.Location = new System.Drawing.Point(13, 42);
+            this.ImageButton.Name = "ImageButton";
+            this.ImageButton.Size = new System.Drawing.Size(116, 23);
+            this.ImageButton.TabIndex = 1;
+            this.ImageButton.Text = "Image Location(s)";
+            this.ImageButton.UseVisualStyleBackColor = true;
+            this.ImageButton.Click += new System.EventHandler(this.ImageButton_Click);
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(13, 71);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(116, 23);
+            this.ConvertButton.TabIndex = 2;
+            this.ConvertButton.Text = "Convert and Swap";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // DebugButton
+            // 
+            this.DebugButton.Location = new System.Drawing.Point(12, 100);
+            this.DebugButton.Name = "DebugButton";
+            this.DebugButton.Size = new System.Drawing.Size(116, 23);
+            this.DebugButton.TabIndex = 3;
+            this.DebugButton.Text = "Debug";
+            this.DebugButton.UseVisualStyleBackColor = true;
+            // 
+            // XMLFileDialog
+            // 
+            this.XMLFileDialog.FileName = "game.xml";
+            // 
+            // TextBoxLog
+            // 
+            this.TextBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxLog.Location = new System.Drawing.Point(136, 13);
+            this.TextBoxLog.Multiline = true;
+            this.TextBoxLog.Name = "TextBoxLog";
+            this.TextBoxLog.ReadOnly = true;
+            this.TextBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.TextBoxLog.Size = new System.Drawing.Size(293, 110);
+            this.TextBoxLog.TabIndex = 4;
+            this.TextBoxLog.WordWrap = false;
+            // 
+            // ImageFileDialog
+            // 
+            this.ImageFileDialog.FileName = "image";
+            this.ImageFileDialog.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 135);
+            this.Controls.Add(this.TextBoxLog);
+            this.Controls.Add(this.DebugButton);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.ImageButton);
+            this.Controls.Add(this.XMLButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button XMLButton;
+        private System.Windows.Forms.Button ImageButton;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Button DebugButton;
+        private System.Windows.Forms.OpenFileDialog XMLFileDialog;
+        private System.Windows.Forms.TextBox TextBoxLog;
+        private System.Windows.Forms.OpenFileDialog ImageFileDialog;
+
     }
 }
 
--- SploderImageSubstitutor/Form1.cs
+++ SploderImageSubstitutor/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
 
 namespace SploderImageSubstitutor
 {
@@ -15,6 +17,73 @@ namespace SploderImageSubstitutor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Init()
+        {
+            this.ImagePaths = new List<string>();
+        }
+
+        private void InitDialog()
+        {
+            XMLFileDialog.Title = "Please find game.xml";
+            ImageFileDialog.Title = "Please pick images";
+        }
+
+        private void Log(string log)
+        {
+            TextBoxLog.Text += String.Format("{0}{1}", log, Environment.NewLine);
+        }
+
+        private string XMLPath;
+        private List<string> XMLData;
+        private List<string> ImagePaths;
+
+        private void XMLButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = XMLFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.XMLPath = XMLFileDialog.FileName;
+                Log(String.Format("XML Location: {0}", this.XMLPath));
+            }
+
+            XDocument xdoc = XDocument.Load(this.XMLPath);
+            this.XMLData = xdoc.Elements().ToList().Select(el => el.ToString()).ToList();
+        }
+
+        private void ImageButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = ImageFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.ImagePaths = ImageFileDialog.FileNames.ToList();
+                this.ImagePaths.ForEach(imagePath => Log(String.Format("Image Location: {0}", imagePath)));
+            }
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            // Create a Backup
+            //string backup = this.XMLPath;
+            //backup += ".bak";
+            //File.Copy(this.XMLPath, backup, true);
+
+            // Credit to Nitin Varpe and Hakam Fostok
+            // https://stackoverflow.com/questions/21325661/convert-image-path-to-base64-string
+            List<string> b64s = this.ImagePaths.Select(imagePath =>
+            {
+                using (Image image = Image.FromFile(imagePath))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        image.Save(ms, image.RawFormat);
+                        byte[] imageBytes = ms.ToArray();
+                        return Convert.ToBase64String(imageBytes);
+                    }
+                }
+            }).ToList();
+            b64s.ForEach(b64 => Log(String.Format("Image Converted: {0}", b64)));
         }
     }
 }
